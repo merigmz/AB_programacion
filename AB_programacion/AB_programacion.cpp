3a2c3a15ad8de72f79b5bc9fa2182b8d4d229009
merigmz <mh.gomez98@gmail.com>
2024-10-01 16:02:23 +0200

    segundo commit
    dhueudh

---
+++ AB_programacion/AB_programacion.cpp
@@ -0,0 +1,12 @@
+﻿// AB_programacion.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "AB_programacion.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
